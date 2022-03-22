1. Fix missing values 2. Disable z-normalization 3. Change the value of nb_epochs and batch_size for all the classifiers 4. Calculates more metrics

classifiers/lstm_fcn.py
@@ -18,6 +18,7 @@ from utils_folder.utils import save_logs, calculate_metrics
 # TODO
 # https://github.com/lilly9117/Sensor-data-classification/blob/77166bec9339965b1bfff58cb477853ef92e7f0d/time_series_classification/classifiers/lstm_fcn.py
 
+
 def squeeze_excite_block(input):
     """
     Create a squeeze-excite block
@@ -25,7 +26,7 @@ def squeeze_excite_block(input):
         input: input tensor
     Returns: a keras tensor
     """
-    filters = input._keras_shape[-1]  # channel_axis = -1 for TF
+    filters = input.shape[-1]  # channel_axis = -1 for TF
 
     se = GlobalAveragePooling1D()(input)
     se = Reshape((1, filters))(se)
@@ -47,8 +48,10 @@ class Classifier_LSTMFCN:
             self.callbacks = None
 
             self.model = self.build_model(input_shape, nb_classes)
-            if verbose:
-                self.model.summary()
+
+            # if verbose:
+            #     self.model.summary()
+
             self.verbose = verbose
             self.model.save_weights(self.output_directory + 'model_init.hdf5')
         return
@@ -104,10 +107,16 @@ class Classifier_LSTMFCN:
         if not tf.test.is_gpu_available:
             print('error')
             exit()
-        # x_val and y_val are only used to monitor the test loss and NOT for training
-        batch_size = 8
+
+        # OLD batch and and epochs
+        # batch_size = 8
         nb_epochs = 100
 
+        # New batch and and epochs
+        batch_size = 128
+        nb_epochs = nb_epochs // 10
+
+        # Was here before
         mini_batch_size = int(min(x.shape[0] / 10, batch_size))
 
         start_time = time.time()

classifiers/rocket.py
@@ -1,15 +1,12 @@
-# Angus Dempster, Francois Petitjean, Geoff Webb
-
-# Dempster A, Petitjean F, Webb GI (2019) ROCKET: Exceptionally fast and
-# accurate time series classification using random convolutional kernels.
-# arXiv:1910.13051
 import time
 
 import numpy as np
 from numba import njit, prange
 from sklearn.linear_model import RidgeClassifierCV
+from sklearn.model_selection import train_test_split
 
-
+from utils_folder.configuration import ConfigClass
+from utils_folder.utils import calculate_metrics
 
 name = "Rocket"
 
@@ -30,7 +27,6 @@ def generate_kernels(input_length, num_kernels, num_channels=1):
     paddings = np.zeros(num_kernels, dtype=np.int32)
 
     for i in range(num_kernels):
-
         _weights = np.random.normal(0, 1, (num_channels, lengths[i]))
 
         a = lengths[:i].sum()
@@ -113,12 +109,7 @@ def apply_kernels(X, kernels, stride=1):
     return _X
 
 
-class RocketRegressor():
-    """
-    This is a class implementing Rocket for time series regression.
-    The code is adapted by the authors from the original Rocket implementation at https://github.com/angus924/rocket
-    """
-
+class Classifier_Rocket:
     def __init__(self,
                  output_directory: str,
                  n_kernels: int = 10000):
@@ -129,18 +120,58 @@ class RocketRegressor():
             output_directory: path to store results/models
             n_kernels: number of random kernels
         """
-        super().__init__(output_directory)
-        print('[{}] Creating Regressor'.format(self.name))
+        config = ConfigClass()
+        config.set_seed()
+
+        # super().__init__(output_directory)
+
         self.name = name
         self.n_kernels = n_kernels
         self.kernels = None
-        self.regressor = RidgeClassifierCV(alphas=np.logspace(-3, 3, 10), normalize=True)
+        self.model = RidgeClassifierCV(alphas=np.logspace(-3, 3, 10), normalize=True)
 
-    def fit(self,
-            x_train: np.array,
-            y_train: np.array,
-            x_val: np.array = None,
-            y_val: np.array = None):
+        self.T = None
+
+        return
+
+    # Taken from Twiesn classifier
+    def compute_state_matrix(self, x_in):
+        # number of instances
+        n = x_in.shape[0]
+
+        # the state matrix to be computed
+        x_t = np.zeros((n, self.T, self.N_x), dtype=np.float64)
+
+        # previous state matrix
+        x_t_1 = np.zeros((n, self.N_x), dtype=np.float64)
+
+        # loop through each time step
+        for t in range(self.T):
+            # get all the time series data points for the time step t
+            curr_in = x_in[:, t, :]
+            # calculate the linear activation
+            curr_state = np.tanh(self.W_in.dot(curr_in.T) + self.W.dot(x_t_1.T)).T
+            # apply leakage
+            curr_state = (1 - self.alpha) * x_t_1 + self.alpha * curr_state
+            # save in previous state
+            x_t_1 = curr_state
+            # save in state matrix
+            x_t[:, t, :] = curr_state
+
+        return x_t
+
+    # Taken from Twiesn classifier
+    @staticmethod
+    def reshape_prediction(y_pred, num_instances, length_series):
+        # reshape so the first axis has the number of instances
+        new_y_pred = y_pred.reshape(num_instances, length_series, y_pred.shape[-1])
+        # average the predictions of instances
+        new_y_pred = np.average(new_y_pred, axis=1)
+        # get the label with maximum prediction over the last label axis
+        new_y_pred = np.argmax(new_y_pred, axis=1)
+        return new_y_pred
+
+    def fit(self, x_train, y_train, x_test, y_test, y_true, iter):
         """
         Fit Rocket
 
@@ -150,34 +181,23 @@ class RocketRegressor():
             x_val: validation data (num_examples, num_timestep, num_channels)
             y_val: validation target
         """
-        start_time = time.perf_counter()
-        print('[{}] Generating kernels'.format(self.name))
+        self.T = x_train.shape[1]
+
+        x_train, x_val, y_train, y_val = \
+            train_test_split(x_train, y_train, test_size=0.3, random_state=(42 + iter))
+
         self.kernels = generate_kernels(x_train.shape[1], self.n_kernels, x_train.shape[2])
-        print('[{}] Applying kernels'.format(self.name))
         x_training_transform = apply_kernels(x_train, self.kernels)
 
-        print('[{}] Training'.format(self.name))
-        self.regressor.fit(x_training_transform, y_train)
-
-
-
-    def predict(self, x: np.array):
-        """
-        Do prediction with Rocket
-
-        Inputs:
-            x: data for prediction (num_examples, num_timestep, num_channels)
-        Outputs:
-            y_pred: prediction
-        """
-        print('[{}] Predicting'.format(self.name))
         start_time = time.perf_counter()
-        print('[{}] Applying kernels'.format(self.name))
-        x_test_transform = apply_kernels(x, self.kernels)
-        y_pred = self.regressor.predict(x_test_transform)
+        self.model.fit(x_training_transform, y_train)
+        duration = time.time() - start_time
 
-        test_duration = time.perf_counter() - start_time
+        x_test = x_test.reshape((x_test.shape[0], x_test.shape[1]))
 
-        print("[{}] Predicting completed, took {}s".format(self.name, test_duration))
+        y_pred = self.model.predict(x_test)
+        y_pred = self.reshape_prediction(y_pred, self.x_test.shape[0], self.T)
 
-        return y_pred
+        df_metrics = calculate_metrics(y_true, y_pred, duration)
+        df_metrics.to_csv(self.output_directory + 'df_metrics.csv', index=False)
+

classifiers/twiesn.py
@@ -34,6 +34,23 @@ class Classifier_TWIESN:
         self.rho_s = [0.55, 0.9, 2.0, 5.0]
         self.alpha = 0.1  # leaky rate
 
+        self.W_in = None
+        self.rho = None
+        self.N_x = None
+        self.connect = None
+        self.scaleW_in = None
+        self.lamda = None
+        self.W_out = None
+
+        self.num_dim = None
+        self.T = None
+        self.x_test = None
+        self.y_true = None
+        self.y_test = None
+        self.x_val = None
+        self.y_val = None
+        self.N = None
+
     def init_matrices(self, iter):
         self.W_in = (2.0 * np.random.rand(self.N_x, self.num_dim) - 1.0) / (2.0 * self.scaleW_in)
 
@@ -42,7 +59,7 @@ class Classifier_TWIESN:
         i = 0
 
         # repeat because could not converge to find eigenvalues
-        while (not converged):
+        while not converged:
             i += 1
 
             # generate sparse, uniformly distributed weights
@@ -65,24 +82,27 @@ class Classifier_TWIESN:
     def compute_state_matrix(self, x_in):
         # number of instances
         n = x_in.shape[0]
+
         # the state matrix to be computed
-        X_t = np.zeros((n, self.T, self.N_x), dtype=np.float64)
+        x_t = np.zeros((n, self.T, self.N_x), dtype=np.float64)
+
         # previous state matrix
-        X_t_1 = np.zeros((n, self.N_x), dtype=np.float64)
+        x_t_1 = np.zeros((n, self.N_x), dtype=np.float64)
+
         # loop through each time step
         for t in range(self.T):
             # get all the time series data points for the time step t
             curr_in = x_in[:, t, :]
             # calculate the linear activation
-            curr_state = np.tanh(self.W_in.dot(curr_in.T) + self.W.dot(X_t_1.T)).T
+            curr_state = np.tanh(self.W_in.dot(curr_in.T) + self.W.dot(x_t_1.T)).T
             # apply leakage
-            curr_state = (1 - self.alpha) * X_t_1 + self.alpha * curr_state
+            curr_state = (1 - self.alpha) * x_t_1 + self.alpha * curr_state
             # save in previous state
-            X_t_1 = curr_state
+            x_t_1 = curr_state
             # save in state matrix
-            X_t[:, t, :] = curr_state
+            x_t[:, t, :] = curr_state
 
-        return X_t
+        return x_t
 
     def reshape_prediction(self, y_pred, num_instances, length_series):
         # reshape so the first axis has the number of instances
@@ -96,9 +116,7 @@ class Classifier_TWIESN:
     def train(self, iter):
         start_time = time.time()
 
-        ################
-        ### Training ###
-        ################
+        # Training
 
         # init the matrices
         self.init_matrices(iter)
@@ -118,9 +136,8 @@ class Classifier_TWIESN:
         # fit the new feature space
         ridge_classifier.fit(new_x_train, new_labels)
 
-        ################
         # Validation
-        ################
+
         # compute state matrix for validation set
         state_matrix = self.compute_state_matrix(self.x_val)
         # add the input to form the new feature space and transform to
@@ -136,9 +153,7 @@ class Classifier_TWIESN:
         # get the train accuracy
         train_acc = df_val_metrics['accuracy'][0]
 
-        ###############
         # Testing
-        ###############
 
         # get the prediction on the test set
         # transform the test set to the new features

temporal_abstraction_f/ucr_to_mts.py
@@ -58,7 +58,7 @@ def fill_transformation1(arr, path, file_type, classes, time_series_number, max_
                 data = lines[index + 1].split(";")
 
                 # TODO
-                #Add the classifier column
+                # Add the classifier column
                 if arr_class is not None:
                     arr_class[int(entity_id)] = int(class_id)
 
@@ -67,7 +67,7 @@ def fill_transformation1(arr, path, file_type, classes, time_series_number, max_
                     parse_data = data[info].split(',')
                     # For each entity, put the state id in the range of start_time to end_time columns
                     arr[int(entity_id)][int(parse_data[0]) - 1: int(parse_data[1]) - 1, time_series_number] = \
-                        int(parse_data[2])+ max_state
+                        int(parse_data[2]) + max_state
 
     if arr_class is not None:
         np.save(output_path + 'type1_' + file_type.lower() + 'classes.npy', arr_class)
@@ -93,7 +93,7 @@ def transformation_1(path, gradient_path, output_path, file_type, number_of_rows
     arr = np.zeros((number_of_rows, number_of_columns, 2))
     arr_class = np.zeros(number_of_rows)
 
-    arr = fill_transformation1(arr, path, file_type, classes, 0, 0,output_path, arr_class)
+    arr = fill_transformation1(arr, path, file_type, classes, 0, 0, output_path, arr_class)
     arr = fill_transformation1(arr,  gradient_path, file_type, classes, 1, max_state, output_path)
 
     # Save the file
@@ -131,7 +131,6 @@ def fill_transformation2(arr, path, file_type, classes, max_state, output_path, 
         np.save(output_path + 'type2_' + file_type.lower() + '_classes.npy', arr_class)
 
     return arr
-
 
 
 def transformation_2(path, gradient_path, output_path, file_type, number_of_rows, number_of_columns, classes):
@@ -221,7 +220,7 @@ def transformation_3(path, gradient_path, output_path, file_type, number_of_rows
     states_df = pd.read_csv(states_path, header=0)
     number_of_states = states_df.shape[0]
     states = states_df.iloc[:, 0]
-    max_state= max(states)
+    max_state = max(states)
 
     states_path_gradient = gradient_path + "train//states.csv"
 

utils_folder/utils.py
@@ -106,13 +106,13 @@ def read_all_datasets(config):
                 df_train = pd.read_csv(root_dir_dataset + '/' + dataset_name + '_TRAIN.tsv', sep='\t', header=None)
                 df_test = pd.read_csv(root_dir_dataset + '/' + dataset_name + '_TEST.tsv', sep='\t', header=None)
 
-            # For us if needed - count how much null there is in the df
-            # df.isnull().sum().sum() != 0:
-
             # Padding missing values
-            df_train = df_train.interpolate(method='linear', limit_direction='both', axis=1)
-            df_test = df_test.interpolate(method='linear', limit_direction='both', axis=1)
+            if df_train.isnull().sum().sum() != 0:
+                df_train = df_train.interpolate(method='linear', limit_direction='both', axis=1)
 
+            if df_test.isnull().sum().sum() != 0:
+                df_test = df_test.interpolate(method='linear', limit_direction='both', axis=1)
+            
             y_train = df_train.values[:, 0]
             y_test = df_test.values[:, 0]
 
@@ -236,7 +236,7 @@ def transform_mts_to_ucr_format():
 
 
 def calculate_metrics(y_true, y_pred, duration, y_true_val=None, y_pred_val=None):
-    res = pd.DataFrame(data=np.zeros((1, 4), dtype=np.float), index=[0],
+    res = pd.DataFrame(data=np.zeros((1, 9), dtype=np.float), index=[0],
                        columns=['precision', 'accuracy', 'recall', 'mcc', 'cohen_kappa', 'duration', 'f1_score_macro',
                                 'f1_score_micro', 'f1_score_weighted'])
 