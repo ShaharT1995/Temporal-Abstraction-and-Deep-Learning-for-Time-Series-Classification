83a4a47d8364327c05979d21faae94369dce69f8
ShaharT1995 <Shaharap@post.bgu.ac.il>
2022-05-12 16:55:57 +0300

    Fixing predict in cnn.py

--- classifiers/cnn.py
+++ classifiers/cnn.py
@@ -36,27 +36,29 @@ class Classifier_CNN:
         if input_shape[0] < 60:
             padding = 'same'
 
-        conv1 = keras.layers.Conv1D(filters=6,kernel_size=7,padding=padding,activation='sigmoid')(input_layer)
+        conv1 = keras.layers.Conv1D(filters=6, kernel_size=7, padding=padding, activation='sigmoid')(input_layer)
         conv1 = keras.layers.AveragePooling1D(pool_size=3)(conv1)
 
-        conv2 = keras.layers.Conv1D(filters=12,kernel_size=7,padding=padding,activation='sigmoid')(conv1)
+        conv2 = keras.layers.Conv1D(filters=12, kernel_size=7, padding=padding, activation='sigmoid')(conv1)
         conv2 = keras.layers.AveragePooling1D(pool_size=3)(conv2)
 
         flatten_layer = keras.layers.Flatten()(conv2)
 
-        output_layer = keras.layers.Dense(units=nb_classes,activation='sigmoid')(flatten_layer)
+        output_layer = keras.layers.Dense(units=nb_classes, activation='softmax')(flatten_layer)
 
         model = keras.models.Model(inputs=input_layer, outputs=output_layer)
 
-        model.compile(loss='mean_squared_error', optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+        model.compile(loss='mean_squared_error', optimizer=keras.optimizers.Adam(learning_rate=0.001),
+                      metrics=['accuracy'])
 
         file_path = self.output_directory + 'best_model.hdf5'
 
-        es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=30, min_delta=0)
+        # es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=30, min_delta=0)
         model_checkpoint = keras.callbacks.ModelCheckpoint(filepath=file_path, monitor='val_loss',
                                                            save_best_only=True)
 
-        self.callbacks = [es, model_checkpoint]
+        # self.callbacks = [es, model_checkpoint]
+        self.callbacks = [model_checkpoint]
         return model
 
     def fit(self, x_train, y_train, x_test, y_test, y_true,  iteration):
@@ -81,6 +83,18 @@ class Classifier_CNN:
         hist = self.model.fit(x_train, y_train, batch_size=mini_batch_size, epochs=nb_epochs,
                               verbose=self.verbose, validation_data=(x_val, y_val), callbacks=self.callbacks)
         learning_time = time.time() - start_time
+
+        # mini_batch_size = 16
+        # nb_epochs = 2000
+        #
+        # # Added lines because model's fit on the testing set - bug in the original code
+        # x_train, x_val, y_train, y_val = \
+        #     train_test_split(x_train, y_train, test_size=0.3, random_state=(42 + iteration))
+        #
+        # start_time = time.time()
+        # hist = self.model.fit(x_train, y_train, batch_size=mini_batch_size, epochs=nb_epochs,
+        #                       verbose=self.verbose, validation_data=(x_val, y_val), callbacks=self.callbacks)
+        # learning_time = time.time() - start_time
 
         self.model.save(self.output_directory + 'last_model.hdf5')
 
